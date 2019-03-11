1a06ba9e4bd7f32f605783c2a23c7f1395703be5
MasterDevX <32103950+MasterDevX@users.noreply.github.com>
2019-03-11 15:20:29 +0200

    Updated uninstallation method

--- RemoveTools.sh
+++ RemoveTools.sh
@@ -6,11 +6,13 @@ echo
 echo -e "\e[32m[*] \e[34mRemoving wrapper script..."
 rm -rf $PREFIX/bin/adb
 echo -e "\e[32m[*] \e[34mRemoving binaries..."
-rm -rf $PREFIX/bin/adb.bin
-rm -rf $PREFIX/bin/fastboot
-rm -rf $PREFIX/bin/fastboot-armeabi
-echo -e "\e[32m[*] \e[34mRemoving workspace directory..."
-rm -rf adbfiles
+rm -rf $PREFIX/bin/adb.bin $PREFIX/bin/fastboot $PREFIX/bin/fastboot-armeabi
+echo -e "\e[32m[*] \e[34mRemoving keys..."
+case `find $HOME/adbfiles -type f | wc -l` in
+0 | 2)
+  rm -rf $HOME/adbfiles ;;
+*)
+  rm -rf $HOME/adbfiles/adbkey $HOME/adbfiles/adbkey.pub ;;
 echo -e "\e[32m[*] \e[34mCleaning up..."
 rm -rf RemoveTools.sh
 echo
